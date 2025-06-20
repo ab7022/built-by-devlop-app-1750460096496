05b34dae6152845d4ef374f60a889deadf89ecb1
Abdul Bayees <bayees1@gmail.com>
2025-06-21 04:25:03 +0530

    Add App.js

---
+++ App.js
@@ -0,0 +1,25 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import HomeScreen from './src/screens/HomeScreen';
+import AddHabitScreen from './src/screens/AddHabitScreen';
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+            animation: 'fade'
+          }}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="AddHabit" component={AddHabitScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
